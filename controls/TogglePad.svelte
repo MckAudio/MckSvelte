f7bb986fa07a6fab399364f2551ad85fc4767fb1
HenriCSHC <henri.meissner@gmx.de>
2021-01-02 15:19:56 +0100

    Added a toggle pad control

--- controls/TogglePad.svelte
+++ controls/TogglePad.svelte
@@ -3,42 +3,16 @@
     import { GetOffsetTop } from "../utils/Tools.svelte";
 
     export let Handler = undefined;
-    export let selected = false;
+    export let active = false;
     export let label = undefined;
 
     let pad = undefined;
     let boxWidth = 0;
-    let active = false;
-    let value = 0.0;
 
     function ButtonClick(_evt) {
-        if (pad === undefined) {
-            return;
-        }
-        let _value = 0.0;
-        if (_evt.type === "mousedown") {
-            _value = 1.0 - _evt.offsetY / pad.clientHeight;
-        } else if (_evt.type === "touchstart") {
-            if (_evt.targetTouches.length > 0) {
-                let _top = GetOffsetTop(pad);
-                _value =
-                    1.0 -
-                    (_evt.targetTouches.item(0).clientY - _top) /
-                        pad.clientHeight;
-            } else {
-                return;
-            }
-        } else {
-            return;
-        }
         if (Handler) {
-            Handler(_value);
+            Handler(!active);
         }
-        value = _value;
-        active = true;
-        window.setTimeout(() => {
-            active = false;
-        }, 50);
     }
 </script>
 
@@ -76,16 +50,18 @@
 <main
     bind:this={pad}
     bind:clientWidth={boxWidth}
-    style="height: {boxWidth}px"
+    style="height: {2.0 * boxWidth}px"
     on:mousedown={(_evt) => ButtonClick(_evt)}
     on:touchstart={(_evt) => ButtonClick(_evt)}>
     {#if active}
         <div
             class="highlight"
-            style="background: radial-gradient(#0099ffff, #f0f0f000 {25.0 + value * 225.0}%);"
-            out:fade />
+            style="background: radial-gradient(#0099ffff, #f0f0f000 200%);"
+            transition:fade="{{duration: 100}}" />
     {/if}
-    {#if label !== undefined}
-        <div class="label">{label}</div>
+    {#if label}
+    <div class="label">
+        {label}
+    </div>
     {/if}
 </main>
